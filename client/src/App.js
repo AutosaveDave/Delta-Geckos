9135889849f6d02e37eb9af8b3e54827f9c67833
David Coppock <106124456+dccoppock@users.noreply.github.com>
2022-11-03 19:29:10 -0400

    MonsterCard component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import TitleNav from "./components/TitleNav/TitleNav";
+import MonsterCard from "./components/MonsterCard/MonsterCard";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <TitleNav position='absolute'></TitleNav>
+        <MonsterCard></MonsterCard>
       </header>
     </div>
   );
