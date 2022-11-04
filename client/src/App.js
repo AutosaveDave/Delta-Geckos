a32dea538840fd8eb619e54c818f586df559483f
Amalthea <rainey22598@gmail.com>
2022-11-04 16:51:42 -0400

    added routes among other things

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
-import TitleNav from "./components/TitleNav.js/TitleNav";
-import MonsterCard from "./components/MonsterCard/MonsterCard";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home.js";
+import LoginSignup from "./Pages/LoginSignup.js";
+import Profile from "./Pages/Profile";
+import Arena from "./Pages/Arena.js";
+import TitleNav from "./components/TitleNav";
+import Footer from "./components/Footer";
+import MonsterCard from "./components/MonsterCard";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <TitleNav position="absolute"></TitleNav>
+        {/* <TitleNav position="absolute"></TitleNav> */}
+        <Router>
+          <TitleNav />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/LoginSignin" element={<LoginSignup />} />
+            <Route path="/Profile" element={<Profile />} />
+            <Route path="/Arena" element={<Arena />} />
+          </Routes>
+          <Footer />
+        </Router>
       </header>
     </div>
   );
--- client/src/components/MonsterCard.js
+++ client/src/components/MonsterCard.js
@@ -4,7 +4,7 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
-import "./style/card.css"
+// import "./style/card.css"
 
 export default function MonsterCard() {
   return (
