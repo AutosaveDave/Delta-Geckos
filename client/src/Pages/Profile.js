faa11cbfcfd3898895b95c1f36b0c44571bd58ed
Amalthea <rainey22598@gmail.com>
2022-11-05 10:06:55 -0400

    profile started

--- client/src/Pages/Profile.js
+++ client/src/Pages/Profile.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <div>
+    <Card sx={{ maxWidth: 345}}>
+      <CardContent>
+        <Typography>
+          Username
+        </Typography>
+        <CardMedia
+          component="img"
+          height="152"
+          image="\assets\characters\2.png"
+          alt="monster 2"
+        />
+        
+
+      </CardContent>
+    </Card>
+    </div>
   )
 }
 
