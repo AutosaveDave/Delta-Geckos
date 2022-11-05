33969c408ae33a769f2e496fc4962317558d18e1
atsede siba <kirossiba@gmail.com>
2022-11-05 10:06:46 -0400

    serverJS

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const server= new ApolloServer({
     typeDefs,
-    resolvers
+   "" resolvers
 });
 
 const app = express();
