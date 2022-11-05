9acb2e42bc2ca1aa47834c0b6ef36c2c237fff91
Amalthea <rainey22598@gmail.com>
2022-11-05 12:39:51 -0400

    arena initiated

--- client/src/components/MonsterCard.js
+++ client/src/components/MonsterCard.js
@@ -4,14 +4,15 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
-import "../style/card.css"
+import "../style/card.css";
 
 export default function MonsterCard() {
   return (
-    <Card  sx={{ width: 142, height: 200 }}
-    >
+    <Card sx={{ width: 142, height: 200 }}>
       <CardActionArea>
-        <Typography sx={{ fontSize: 13, marginLeft: 1, marginBottom: -3 }}>Oscar The Marcus</Typography>
+        <Typography sx={{ fontSize: 13, marginLeft: 1, marginBottom: -3 }}>
+          Oscar The Marcus
+        </Typography>
         <CardMedia
           component="img"
           height="152"
@@ -19,15 +20,19 @@ export default function MonsterCard() {
           alt="monster 2"
         />
         <CardContent>
-          <Typography sx={{ fontSize: 12, marginTop: -6 }} variant="body2" color="text.secondary">
-          <br></br>
+          <Typography
+            sx={{ fontSize: 12, marginTop: -6 }}
+            variant="body2"
+            color="text.secondary"
+          >
+            <br></br>
             Watch out.
-          <br></br>
+            <br></br>
             He'll get'cha.
           </Typography>
           <Typography sx={{ fontSize: 10, marginTop: 2, marginLeft: 11 }}>
             A/D
-          </Typography>  
+          </Typography>
         </CardContent>
       </CardActionArea>
     </Card>
