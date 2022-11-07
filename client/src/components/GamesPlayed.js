6a21a9d9afcba60c4de0c1ba217e0ed8d6a04bd7
Merkisha Wilkinson <merkisha94@gmail.com>
2022-11-07 17:13:24 -0500

    profile pge set up initial

--- client/src/components/GamesPlayed.js
+++ client/src/components/GamesPlayed.js
@@ -1,8 +1,7 @@
 import * as React from "react";
 import { styled } from "@mui/material/styles";
-import Grid from "@mui/material/Unstable_Grid2";
 import Paper from "@mui/material/Paper";
-import Box from "@mui/material/Box";
+import { Typography } from "@mui/material";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -13,13 +12,5 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 export default function GamesPlayed() {
-  return (
-    <Box sx={{ width: "90%" }}>
-      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        <Grid xs={6}>
-          <Item>Games Played</Item>
-        </Grid>
-      </Grid>
-    </Box>
-  );
+  return <Item>Games Played</Item>;
 }
