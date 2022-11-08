dba5ba6959ad1b12ae87eb4b7bf4321c5f840784
atsede siba <kirossiba@gmail.com>
2022-11-07 20:15:56 -0500

    loginindex

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home.js";
-import LoginSignup from "./Pages/LoginSignup.js";
+import { Route, Routes } from "react-router-dom";
+import ResponsiveAppBar from "./components/TitleNav"
+import LoginSignup from "./Pages/LoginSignup";
+import Arena from "./Pages/Arena";
 import Profile from "./Pages/Profile";
-import Arena from "./Pages/Arena.js";
-import TitleNav from "./components/TitleNav";
-import Footer from "./components/Footer";
+
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <TitleNav position="absolute"></TitleNav> */}
-        <Router>
-          <TitleNav />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/LoginSignin" element={<LoginSignup />} />
-            <Route path="/Profile" element={<Profile />} />
-            <Route path="/Arena" element={<Arena />} />
-          </Routes>
-          <Footer />
-        </Router>
-      </header>
+       <ResponsiveAppBar/>
+     <Routes>
+      <Route path ="/" element ={<Arena/>}/>
+      <Route path ="/" element ={<LoginSignup/>}/>
+      <Route path ="/" element ={<Profile/>}/>
+     </Routes>
+        
     </div>
   );
 }
---
+++ client/src/Pages/LoginSignup.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Login from "./Login/components;"
+import Signup from "./Login/components;"
+
+
+const LoginSignup=()=>{
+   return <div className="App"> Login/Signup</div>
+
+};
+
+export default LoginSignup;
