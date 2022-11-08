34203550965580a6556218edc63d9a280fd019d3
Merkisha Wilkinson <merkisha94@gmail.com>
2022-11-08 13:18:28 -0500

    auth in progress, added utils in client folder

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -10,6 +10,8 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+// import { useMutation } from "@apollo/client";
+// import { LOGIN_USER } from "../utils/mutations";
 
 const theme = createTheme();
 
