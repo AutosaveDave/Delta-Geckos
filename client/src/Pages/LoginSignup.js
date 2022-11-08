e90c490256215bab4c04e569728b43a6578d653e
Merkisha Wilkinson <merkisha94@gmail.com>
2022-11-07 19:47:25 -0500

    login and signup started

---
+++ client/src/Pages/LoginSignup.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Login from "../components/Login";
+import Signup from "../components/Signup";
+
+function LoginSignup() {
+  return <div>Home</div>;
+}
+
+export default LoginSignup;
