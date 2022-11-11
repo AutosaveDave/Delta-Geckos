c67ab4036530690019d89b9bddd9ea7bb6c9ae89
atsede siba <kirossiba@gmail.com>
2022-11-11 18:05:22 -0500

    added typwriter effect to the Home page

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Hero from '../images/hero1.png';
+import Hero from '../images/hero.png';
+import Typewriter from 'typewriter-effect';
 import "../style/Home.css";
 
 
@@ -9,9 +10,28 @@ function Home() {
 return (
     <div> 
     <img src={Hero} alt="background"/>
-
+    <div className='Home'>
+      <Typewriter 
+      onInit={(typewriter)=>{
+        typewriter.typeString("ARE YOU READY TO WIN GOLD!?!?")
+        .callFunction(()=>{
+          alert("Let's go!");
+        })
+        .pauseFor(2000)
+        .deleteAll()
+        .typeString("Enter the arena and battle with other users monsters to build your hoard of riches!")
+        .pauseFor(2000)
+        .deleteAll()
+        .typeString("In this two plaer game, you will have a hand of five monsters to choose from. Once both parties have chosen their fighters their unique strengths will determine the victor.")
+        .pauseFor(2000)
+        .deleteAll()
+        .typeString("How much gold can you pile up?")
+        .start();
+      }}
+/>
+    </div>
 </div>
   );
  }
 
-export default Home
+export default Home;
